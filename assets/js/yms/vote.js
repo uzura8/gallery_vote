514e30a4571b4bbd95e32070c8fb63f9205fac5a
shingo <uzuranoie@gmail.com>
2014-09-09 08:25:25 +0900

    apprise, jgrowl を実装

--- assets/js/yms/vote.js
+++ assets/js/yms/vote.js
@@ -8,6 +8,7 @@ $(function () {
     })
     .on('slide', function (event, index, slide) {
       // Gallery slide event handler
+      closeApprise();
       var id = index + 1;
       var voteBtn = $(this).find('.btn_vote');
       changeBtnStatus(voteBtn, checkVoted(id) ? 'voted' : 'default');
@@ -28,21 +29,10 @@ $(function () {
 });
 
 $(document).on('click', '.btn_vote', function(){
-  var onslide = $(this).data('onslide') ? Boolean($(this).data('onslide')) : false;
-
-  var entryId = $(this).data('entry_id') ? parseInt($(this).data('entry_id')) : 0;
-  if (!entryId) entryId = $('#gallery_onslide').val();
-  if (!entryId) return false;
-
-  changeBtnStatus(this);
-  if (onslide) changeBtnStatus('#btn_vote_' + entryId);
-
-  var votedIds = $('#voted_ids').val();
-  var addValue = '';
-  if (votedIds) addValue = ',';
-  addValue += entryId;
-  $('#voted_ids').val(votedIds + addValue);
-
+  var btnVoteSelector = this;
+  apprise('投票しますか？', {'verify':true}, function(r) {
+    if (r == true) executeVote(btnVoteSelector);
+  });
   return false;
 });
 
@@ -83,4 +73,35 @@ function checkVoted(id)
   var votedIdString = $('#voted_ids').val();
   if (!votedIdString) return false;
   return $.inArray(id, votedIdString.split(',')) !== -1;
+}
+
+function executeVote(selfSelector)
+{
+  var onslide = $(selfSelector).data('onslide') ? Boolean($(selfSelector).data('onslide')) : false;
+
+  var entryId = $(selfSelector).data('entry_id') ? parseInt($(selfSelector).data('entry_id')) : 0;
+  if (!entryId) entryId = $('#gallery_onslide').val();
+  if (!entryId) return false;
+
+  changeBtnStatus(selfSelector);
+  if (onslide) changeBtnStatus('#btn_vote_' + entryId);
+
+  var votedIds = $('#voted_ids').val();
+  var addValue = '';
+  if (votedIds) addValue = ',';
+  addValue += entryId;
+  $('#voted_ids').val(votedIds + addValue);
+
+  showMessage('投票しました。');
+}
+
+function showMessage(msg)
+{
+  $.jGrowl(msg);
+}
+
+function closeApprise()
+{
+  $('.appriseOverlay').remove();
+  $('.appriseOuter').remove();
 }
