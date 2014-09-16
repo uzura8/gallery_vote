e53cb99cc158c4f65293a794d56e3b10e76e1467
shingo <uzuranoie@gmail.com>
2014-09-17 07:58:03 +0900

    API で投票結果を送信するようにした

--- assets/js/yms/util.js
+++ assets/js/yms/util.js
@@ -77,7 +77,6 @@ function checkVoted(id)
 function executeVote(selfSelector)
 {
   var onslide = $(selfSelector).data('onslide') ? Boolean($(selfSelector).data('onslide')) : false;
-
   var entryId = $(selfSelector).data('entry_id') ? parseInt($(selfSelector).data('entry_id')) : 0;
   if (!entryId) entryId = $('#gallery_onslide').val();
   if (!entryId) return false;
@@ -92,15 +91,40 @@ function executeVote(selfSelector)
     return false;
   }
 
-  votedIds = addCookie(entryId);
-  if (!votedIds) return;
+  var selfSelectorHtml = $(selfSelector).html();
+  var postData = {'id': entryId};
+  $.ajax({
+    url : getConf('apiUrl'),
+    type : 'POST',
+    dataType: 'jsonp',
+    jsonpCallback: 'response',
+    data : postData,
+    timeout: 10000,
+    beforeSend: function(xhr, settings) {
+      GL.execute_flg = true;
+      setLoading(null, selfSelector, 'btn_loading_image');
+    },
+    complete: function(xhr, textStatus) {
+      GL.execute_flg = false;
+    },
+    success: function(result){
+      votedIds = addCookie(entryId);
+      if (!votedIds) return;
 
-  changeBtnStatus(selfSelector);
-  setVotedCount(votedIds.length);
-  if (onslide) changeBtnStatus('#btn_vote_' + entryId);
-  if (!checkEnableToVote(votedIds)) changeVoteCompleteView();
+      removeLoading(selfSelector, 'btn_loading_image', false);
+      changeBtnStatus(selfSelector);
+      setVotedCount(votedIds.length);
+      if (onslide) changeBtnStatus('#btn_vote_' + entryId);
+      if (!checkEnableToVote(votedIds)) changeVoteCompleteView();
 
-  showMessage('投票しました。');
+      showMessage('投票しました。');
+    },
+    error: function(result){
+      removeLoading(selfSelector, 'btn_loading_image');
+      $(selfSelector).html(selfSelectorHtml);
+      showMessage('投票に失敗しました。', 'error');
+    }
+  });
 }
 
 function getVotedIds()
@@ -130,6 +154,63 @@ function closeApprise()
   $('.appriseOuter').remove();
 }
 
+function getLoadingImageTag()
+{
+  var isEncloseBlock = (arguments.length > 0) ? arguments[0] : false;
+  var blockSelector = (arguments.length > 1) ? arguments[1] : '';
+
+  var imageTag = '<img src="' + getUrl('assets/img/loading_s.gif') + '">';
+  if (!isEncloseBlock) return imageTag;
+
+  var blockTag = '<div class="loading_image"'
+  if (blockSelector) blockTag += ' id="' + blockSelector + '"';
+  blockTag += '>' + imageTag + '</div>';
+
+  return blockTag;
+}
+
+function removeLoadingBlock()
+{
+  var loadingBlockId = (arguments.length > 0) ? arguments[0] : '';
+
+  if (loadingBlockId) {
+    $('#' + loadingBlockId).remove();
+    return;
+  }
+
+  $('.loading_image').remove();
+}
+
+function removeLoading()
+{
+  var trigerSelector = (arguments.length > 0) ? arguments[0] : '';
+  var loadingBlockId = (arguments.length > 1) ? arguments[1] : '';
+  var isBtnEnabled = (arguments.length > 2) ? Boolean(arguments[2]) : true;
+  var isRemoveTrigerSelector = (arguments.length > 3) ? Boolean(arguments[3]) : false;
+
+  if (trigerSelector) {
+    if (isBtnEnabled) $(trigerSelector).attr('disabled', false);
+    if (isRemoveTrigerSelector) {
+      $(trigerSelector).remove();
+      return;
+    }
+  }
+  removeLoadingBlock(loadingBlockId);
+}
+
+function setLoading(blockSelector)
+{
+  var trigerSelector = (arguments.length > 1) ? arguments[1] : '';
+  var loadingBlockSelector = (arguments.length > 2) ? arguments[2] : '';
+
+  if (trigerSelector) {
+    $(trigerSelector).attr('disabled', true);
+    $(trigerSelector).html(getLoadingImageTag(true, loadingBlockSelector));
+  } else {
+    $(blockSelector).append(getLoadingImageTag(true, loadingBlockSelector));
+  }
+}
+
 function showMessage(msg)
 {
   var viewType = (arguments.length > 1) ? arguments[1] : 'success';
@@ -140,6 +221,10 @@ function showMessage(msg)
   });
 }
 
+function getUrl(projectUri) {
+  return getConf('basePath') + projectUri;
+}
+
 function getConf(key) {
-  return  GL.configs[key];
+  return GL.configs[key];
 }
